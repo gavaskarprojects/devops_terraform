eede4aef5669cdb8bd619ba124747b6687c9f92b
gavaskarprojects <85656347+gavaskarprojects@users.noreply.github.com>
2024-08-23 01:56:30 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -15,7 +15,7 @@ resource "alicloud_vswitch" "main" {
   # CIDR block of the VSwitch
   cidr_block        = "10.1.0.0/24"
   # Zone
-  availability_zone = "cn-hangzhou-b"
+  availability_zone = "ap-southeast-1a"
   # Dependent resource (this dependent resource will be created first)
   depends_on = [alicloud_vpc.main]
 }
